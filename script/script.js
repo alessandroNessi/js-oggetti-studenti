c7bb98b6887e9f21400591e249df00bd842fd414
alessandroNessi <alessandronessi@msn.com>
2021-09-07 16:29:43 +0200

    esercizio finito e commentato

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/*print students in ul "userList" */
 function printStudents(){
     document.getElementById("userList").innerHTML="";
     studenti.forEach(function (element, index){
@@ -7,6 +8,8 @@ function printStudents(){
         document.getElementById("userList").innerHTML+=`<li>${element.eta}</li><hr>`;
     });
 }
+
+//declaring vars
 var studente1 ={
     "nome": "marco",
     "cognome": "rossi",
@@ -23,17 +26,23 @@ var studente3 = {
     "eta": 25
 };
 var studenti=[studente1, studente2, studente3];
-console.log(studenti);
+//end declaring vars
+
+// print students in the ul proprietyList with an adjacent input for the new insert
 for(var key in studente1){
     document.getElementById("proprietyList").innerHTML+=`<li>${key}:<input id="${key}" type="text"></li>`;
 }
+
+//prints the students that we placed in the var declaration into "userList"
 printStudents();
+
+//gives the click event to the button and add and check new students
 document.getElementById("newStudent").addEventListener("click", function(){
     let nome=document.getElementById("nome").value, cognome, eta;
-    if(nome.length>0 && nome.length<50 && isNaN(parseInt(nome))){
+    if(nome.length>0 && nome.length<50 && isNaN(parseInt(nome))){//check 0<name<50 and is not a numb
         let cognome=document.getElementById("cognome").value;
-        if(cognome.length>0 && cognome.length<50 && isNaN(parseInt(cognome))){
-            let eta=parseInt(document.getElementById("eta").value);
+        if(cognome.length>0 && cognome.length<50 && isNaN(parseInt(cognome))){//check 0<cognome<50 and is not a numb
+            let eta=parseInt(document.getElementById("eta").value);//check eta is a numb
             if(eta>0 && !isNaN(eta)){
                 let newstudent ={
                     "nome": nome,
@@ -45,4 +54,7 @@ document.getElementById("newStudent").addEventListener("click", function(){
             }   
         }
     }
+    document.getElementById("nome").value="";
+    document.getElementById("cognome").value="";
+    document.getElementById("eta").value="";
 });
